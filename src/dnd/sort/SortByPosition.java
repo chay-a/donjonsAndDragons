1dbf3b844283d5db3b4fb691f92f443b5ffe0655
Chay Auriane <chaya.07@protonmail.com>
2022-05-16 11:48:36 +0200

    refactor : comment compare method

--- src/dnd/sort/SortByPosition.java
+++ src/dnd/sort/SortByPosition.java
@@ -6,6 +6,12 @@ import java.util.Comparator;
 
 public class SortByPosition implements Comparator<CharacterInGame> {
 
+    /**
+     * Compare two characterInGame's position
+     * @param character1 the first object to be compared.
+     * @param character2 the second object to be compared.
+     * @return int
+     */
     public int compare(CharacterInGame character1, CharacterInGame character2) {
         return Integer.compare(character1.getPosition(), character2.getPosition());
     }
